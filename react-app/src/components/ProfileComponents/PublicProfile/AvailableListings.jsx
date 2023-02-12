72b33a942c8f3308d392185fe12fe9f32125fad2
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-11 21:46:31 -0500

    ok

--- react-app/src/components/ProfileComponents/PublicProfile/AvailableListings.jsx
+++ react-app/src/components/ProfileComponents/PublicProfile/AvailableListings.jsx
@@ -6,7 +6,7 @@ const AvailableListings = ({ items }) => {
     return (
         <div className="listings-tab-container">
             <div className="filters">
-                <label htmlFor="Checkbox"><input type="checkbox" name="a_filter_checkbox" /></label>
+                <input type="checkbox" name="a_filter_checkbox" />
 
                 <input type="checkbox" name="a_filter_checkbox" />
                 <input type="checkbox" name="a_filter_checkbox" />

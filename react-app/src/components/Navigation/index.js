013920820bc12ce682b7bc103bdcf47fcfbcb34c
stevenpicazo <stevenpicazo01@gmail.com>
2023-02-11 23:21:57 -0800

    nav bar styling

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
+	const [search, setSearch] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(`Searching for: ${search}`);
+	};
+
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+	};
+
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
 		<div className='navbar'>
-			<div>
+			<div className='home-link'>
 				<NavLink exact to="/">Home</NavLink>
 			</div>
-			{isLoaded && (
-				<div>
-					<ProfileButton user={sessionUser} />
-				</div>
-			)}
+
+			<div className="search-container">
+				<input type="text" placeholder="Search" className="search-input" />
+				<button className="search-button">Search</button>
+			</div>
+			<div className='right-side-container'>
+
+				<button className=''>Sell</button>
+				<NavLink exact to="/items">Shop</NavLink>
+				{isLoaded && (
+					<div>
+						<ProfileButton user={sessionUser} />
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }

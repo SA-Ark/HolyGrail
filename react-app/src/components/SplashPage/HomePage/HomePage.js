7be9849e028b830ef426fb910324204729a43cdf
stevenpicazo <stevenpicazo01@gmail.com>
2023-02-12 22:09:15 -0800

    splash page videos height and width updated

--- react-app/src/components/SplashPage/HomePage/HomePage.js
+++ react-app/src/components/SplashPage/HomePage/HomePage.js
@@ -9,6 +9,8 @@ const SplashPlage = () => {
     const dispatch = useDispatch()
     const [videoPlaying, setVideoPlaying] = useState(false)
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndexDeals, setCurrentIndexDeals] = useState(0);
+
 
     // const items = utils.deNormalize(useSelector(store => store.items.allItems))
     const user = useSelector(store => store.session?.user)
@@ -21,25 +23,45 @@ const SplashPlage = () => {
 
     const handlePlay = () => {
         setVideoPlaying(true)
-    };
+    }
 
     const handleNext = () => {
         setCurrentIndex((currentIndex + 1) % 5)
-    };
+    }
 
     const handlePrev = () => {
         setCurrentIndex((currentIndex + 4) % 5)
-    };
+    }
+
+    const handleNextDeals = () => {
+        setCurrentIndexDeals((currentIndexDeals + 1) % 5)
+    }
+
+    const handlePrevDeals = () => {
+        setCurrentIndexDeals((currentIndexDeals + 4) % 5)
+    }
 
     const itemCards = Object.values(items).slice(currentIndex, currentIndex + 5).map(item => {
+
         return (
             <ItemCard key={item.id} item={item} />
         )
     })
 
+    const dealCards = Object.values(items)
+        .filter(item => item.price < 100)
+        .slice(currentIndexDeals, currentIndexDeals + 5)
+        .map(item => {
+            return (
+                <ItemCard key={item.id} item={item} />
+            )
+        });
+
     return (
         <div className="splash-page-container">
+
             <div className="filters-bar"></div>
+
             <div className="splash-video-conatiner">
                 <video
                     className='splash-page-video'
@@ -49,6 +71,7 @@ const SplashPlage = () => {
                     loop
                     playsInline
                 ></video>
+
                 <div className='splash-video-title'>
                     <h1>THE PLATFORM FOR PERSONAL STYLE</h1>
                 </div>
@@ -56,19 +79,36 @@ const SplashPlage = () => {
                     Buy, sell, discover authenticated pieces from the world's top brands.
                 </div>
             </div>
-            <div className='splash-carousel-container'>
-                <div className='staff-picks'>
-                    <div className='arrow prev' onClick={handlePrev}>&lt;</div>
-                    <div className='carousel-item-container'>
-                        {itemCards.length > 0 ? itemCards : null}
+
+            <div className='splash-carousel-wrapper'>
+                <div className='splash-carousel-container'>
+                    <div className='staff-picks'>
+                        <div className='arrow prev' onClick={handlePrev}>&lt;</div>
+                        <div className='carousel-item-container'>
+                            {itemCards.length > 0 ? itemCards : null}
+                        </div>
+                        <div className='arrow next' onClick={handleNext}>&gt;</div>
                     </div>
-                    <div className='arrow next' onClick={handleNext}>&gt;</div>
                 </div>
-                <div className='Deals'></div>
+
+                <span className='deals-text'> Deals: Under $100 </span>
+                <div className='splash-carousel-container'>
+                    <div className='deals'>
+
+                        <div className='arrow prev' onClick={handlePrevDeals}>&lt;</div>
+                        <div className='carousel-item-container'>
+                            {dealCards.length > 0 ? dealCards : null}
+                        </div>
+                        <div className='arrow next' onClick={handleNextDeals}>&gt;</div>
+                    </div>
+                </div>
             </div>
-        
+
         </div >
     )
 }
 
 export default SplashPlage;
+
+
+

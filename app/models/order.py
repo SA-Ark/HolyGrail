d20c29cc140583075f97a1b1ff6d7f99170d6016
Arko Chakrabarty <c.arko.business@gmail.com>
2023-02-13 23:21:18 -0800

    foreign key

--- app/models/order.py
+++ app/models/order.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .db import db, User, Item, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
--- app/models/review.py
+++ app/models/review.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, User, Item, environment, SCHEMA
 from datetime import datetime
 
 

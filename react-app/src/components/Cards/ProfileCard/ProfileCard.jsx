579a65259297dcf6e1789a19ca06e6eeb5c4c184
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2023-02-14 17:03:01 -0500

    profile card

--- react-app/src/components/Cards/ProfileCard/ProfileCard.jsx
+++ react-app/src/components/Cards/ProfileCard/ProfileCard.jsx
@@ -1,4 +1,3 @@
-//This will live on single item page
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { thunkLoadCurrReviews } from "../../../store/reviews";
@@ -7,9 +6,9 @@ const ProfileCard = ({ item }) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
     const reviews = useSelector(state => state?.reviews?.allReviews);
-    console.log("ITEM --->", item);
-    console.log('USER --->', user);
-    console.log("REVIEWS --->", reviews);
+    // console.log("ITEM --->", item);
+    // console.log('USER --->', user);
+    // console.log("REVIEWS --->", reviews);
 
     useEffect(() => {
         dispatch(thunkLoadCurrReviews(user?.id));
@@ -17,11 +16,22 @@ const ProfileCard = ({ item }) => {
 
     return (
         <>
-            <div>
-                {}
-            </div>
-            <div>
-                Seller Stars
+            <div className="profile-card-container">
+                <div>
+                    {reviews.avg_rating} SELLER AVG STAR RATING HERE
+                </div>
+                <div>
+                    {reviews.num_reviews} NUM SELLER REVIEWS HERE
+                </div>
+                <div>
+                    {reviews.num_transactions} Transactions
+                </div>
+                <div>
+                    {reviews.items_for_sale} items for sale
+                </div>
+                <div>
+                    SELLER BADGES HERE
+                </div>
             </div>
         </>
     )

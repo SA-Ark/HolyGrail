e0f60f6ecc415de0635ef0fc3564e951820b9683
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-14 12:51:28 -0500

    ok

--- react-app/src/components/ProfileComponents/UserDashboard/Tabs/PurchasesTab.jsx
+++ react-app/src/components/ProfileComponents/UserDashboard/Tabs/PurchasesTab.jsx
@@ -1,4 +1,7 @@
+import { deNormalize } from "../../../../store/utils";
+
 const PurchasesTab = ({purchases}) => {
+    const purchaseList = deNormalize()
 
     return (
         <div className="purchases-container">

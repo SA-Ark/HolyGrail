2cdc70b07d92fb40f31258ff6d7cda7f9975fee2
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2023-02-13 23:32:19 -0500

    merge commit

--- react-app/src/components/ItemsDisplayComponents/SingleItemPage/SingleItemPage.jsx
+++ react-app/src/components/ItemsDisplayComponents/SingleItemPage/SingleItemPage.jsx
@@ -21,7 +21,7 @@ const SingleItemPage = () => {
 
   return (
     <div className="single-item-page-container">
-      <div className="large-img-container">
+      <div className="img-container">
         {item
           ? <ImageCarousel images={item.images?.length ? item.images : null}/>
           : null}
@@ -29,17 +29,17 @@ const SingleItemPage = () => {
       
       <div className="item-info-buttons-container">
         <span id="item-name">{item.name}</span>
-        <span>Size {item.size}</span>
-        <span>Color {item.color}</span>
-        <span>Condition {item.condition}</span>
+        <span id="item-size">Size {item.size}</span>
+        <span id="item-color">Color {item.color}</span>
+        <span id="item-condition">Condition {item.condition}</span>
         <span id="item-price">${item.price}</span>
-        <span>+${item.shipping_cost} Shipping - Europe to United States</span>
-        <button>Purchase</button>
-        <button>Offer</button>
-        <button>Message</button>
-        <span>Description<br />{item.description}</span>
-        <span>Tags</span>
-        <span>Posted on {item.created_at}</span>
+        <span id="item-shipping">+${item.shipping_cost} Shipping - Europe to United States</span>
+        <button id="purchase-btn">Purchase</button>
+        <button id="offer-btn">Offer</button>
+        <button id="message-btn">Message</button>
+        <span id="item-description">Description<br />{item.description}</span>
+        <span id="item-tags">Tags</span>
+        <span id="item-created-at">Posted on {item.created_at}</span>
       </div>
     </div>
   )

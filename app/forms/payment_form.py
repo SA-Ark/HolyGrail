c76bc15476b024eedc2d7e4151701da501cf696c
stevenpicazo <stevenpicazo01@gmail.com>
2023-02-15 23:22:19 -0800

    Sign up validations work

--- app/forms/payment_form.py
+++ app/forms/payment_form.py
@@ -1,12 +1,64 @@
 from flask_wtf import FlaskForm
 from wtforms import DateField, IntegerField, StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Length, Regexp, NumberRange
 
 class PaymentForm(FlaskForm):
-    order_total = IntegerField("Order Total", validators=[DataRequired()])
-    card_number = StringField("Card Number", validators=[DataRequired()])
-    expiry =DateField("Expiry", validators=[DataRequired()])
-    cvc = IntegerField("CVC", validators=[DataRequired()])
-    card_country = StringField("Card Country", validators=[DataRequired()])
-    card_zip = IntegerField("Card Zip", validators=[DataRequired()])
-    shipping_address = StringField("Shipping Address", validators=[DataRequired()])
+    order_total = IntegerField(
+        "Order Total",
+        validators=[
+            DataRequired(message="Please enter an order total."),
+            NumberRange(min=0, message="Order total must be a positive number.")
+        ]
+    )
+    card_number = StringField(
+        "Card Number",
+        validators=[
+            DataRequired(message="Please enter a card number."),
+            Length(min=16, max=16, message="Card number must be 16 digits long."),
+            Regexp('^[0-9]*$', message="Card number can only contain digits.")
+        ]
+    )
+    expiry = DateField(
+        "Expiry",
+        format="%m/%y",
+        validators=[
+            DataRequired(message="Please enter a card expiry date."),
+        ]
+    )
+    cvc = IntegerField(
+        "CVC",
+        validators=[
+            DataRequired(message="Please enter a CVC number."),
+            Length(min=3, max=4, message="CVC number must be 3 or 4 digits long."),
+            Regexp('^[0-9]*$', message="CVC number can only contain digits.")
+        ]
+    )
+    card_country = StringField(
+        "Card Country",
+        validators=[
+            DataRequired(message="Please enter a card country."),
+            Length(max=50, message="Card country must be less than 50 characters.")
+        ]
+    )
+    card_zip = IntegerField(
+        "Card Zip",
+        validators=[
+            DataRequired(message="Please enter a card zip code."),
+            NumberRange(min=1, message="Card zip code must be a positive number.")
+        ]
+    )
+    shipping_address = StringField(
+        "Shipping Address",
+        validators=[
+            DataRequired(message="Please enter a shipping address."),
+            Length(max=100, message="Shipping address cannot be longer than 100 characters.")
+        ]
+    )
+
+    # order_total = IntegerField("Order Total", validators=[DataRequired()])
+    # card_number = StringField("Card Number", validators=[DataRequired()])
+    # expiry =DateField("Expiry", validators=[DataRequired()])
+    # cvc = IntegerField("CVC", validators=[DataRequired()])
+    # card_country = StringField("Card Country", validators=[DataRequired()])
+    # card_zip = IntegerField("Card Zip", validators=[DataRequired()])
+    # shipping_address = StringField("Shipping Address", validators=[DataRequired()])
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email, ValidationError, Length, Regexp
 from app.models import User
-
 
 def user_exists(form, field):
     # Checking if user exists
@@ -22,18 +21,54 @@ def username_exists(form, field):
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
-    first_name = StringField('first name', validators=[DataRequired()])
-    last_name = StringField('last name', validators=[DataRequired()])
-    street_address = StringField('street address', validators=[DataRequired()])
-    city = StringField('city', validators=[DataRequired()])
-    state = StringField('state', validators=[DataRequired()])
-    postal_code = StringField('postal code', validators=[DataRequired()])
-    country = StringField('country', validators=[DataRequired()])
-    gender_style = BooleanField('gender style')
-    shirt_size = BooleanField('shirt size')
-    pant_size = BooleanField('pant size')
-    shoe_size = BooleanField('shoe size')
-    
+        'Username', validators=[
+            DataRequired(message="Please enter a username."), username_exists,
+            Length(min=4, max=25, message="Username must be between 4 and 25 characters long."),
+            Regexp('^[A-Za-z][A-Za-z0-9_]*$', message="Username can only contain letters, numbers, and underscores.")
+        ]
+    )
+    email = StringField('email', validators=[DataRequired(), user_exists, Length(
+        min=5, max=30, message='Username must be between 3 and 30 characters long.'
+    )])
+    password = StringField(
+        'Password', validators=[DataRequired(message="Please enter a password."),Length(
+        min=3, max=50, message='Password must be between 3 and 50 characters long.'
+    )])
+    first_name = StringField(
+        'First Name', validators=[DataRequired(message="Please enter your first name."),
+            Length(min=1, max=100, message="First Name must be between 1 and 100 characters.")]
+    )
+    last_name = StringField(
+        'Last Name', validators=[DataRequired(message="Please enter your last name."),
+            Length(min=1, max=100, message="Last Name must be between 1 and 100 characters.")]
+    )
+    street_address = StringField(
+        'Street Address', validators=[DataRequired(message="Please enter your street address."),
+            Length(min=3, max=100, message="Street address must be between 3 and 100 characters.")]
+    )
+    city = StringField(
+        'City', validators=[DataRequired(message="Please enter your city."),
+            Length(max=50, message="City must be less than 50 characters.")]
+    )
+    state = StringField(
+        'State', validators=[DataRequired(message="Please enter your state."),
+            Length(max=50, message="State must be less than 50 characters.")]
+    )
+    postal_code = StringField(
+        'Postal Code', validators=[ DataRequired(message="Please enter your postal code."),
+            Length(min=1, max=10, message="Postal code cannot be longer than 10 characters.")]
+    )
+    country = StringField(
+        'Country', validators=[
+            DataRequired(message="Please enter your country."),
+            Length(min=1, max=50, message="Country must be less than 50 characters.")]
+    )
+    gender_style = StringField(
+        'Gender Style', validators=[DataRequired(message="Please select a gender style."),
+            Length(min=1, max=50, message="Character limit reached.")],
+    )
+    shirt_size = StringField(
+        'Shirt Size', validators=[DataRequired(message="Please select a shirt size.")],
+    )
+    pant_size = StringField('Pant Size')
+    shoe_size = StringField('Shoe Size')

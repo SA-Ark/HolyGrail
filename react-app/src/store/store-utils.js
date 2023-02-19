12ef4dcdbe2d773bad7df8653cd9063a499402bd
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-18 20:28:37 -0500

    ok

--- react-app/src/store/store-utils.js
+++ react-app/src/store/store-utils.js
@@ -5,39 +5,32 @@ export const spreadPayments = (payments)=>{
     for (let payment of payments){
         const newPayment = {}
 
-        console.log(payment)
-
         const {order, item} = payment
         newPayment.item = {...item}
         newPayment.order = {...order}
         newState[count] = newPayment
-        console.log(order, item, "MAYBE?")
         count++
     }
     return newState
 }
 
 export const spreadFavorites = (favorites)=>{
-    console.log(favorites, "favorites IN UTIL")
     const newState = {}
     let count = 0
     for (let favoriteItem of favorites){
         const newFavorite = {}
 
-
-
         const {favorite, item} = favoriteItem
         newFavorite.item = {...item}
         newFavorite.favorite = {...favorite}
         newState[count] = newFavorite
-        console.log(favorite, item, "MAYBE?")
         count++
     }
     return newState
 }
 
 export const spreadItems = (items)=>{
-   
+
     const newState = {}
     let count = 0
     for (let item of Object.values(items)){
